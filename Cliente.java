cb8f6b7c0a808fb19fac29cb3025c0783e7add3c
Victor Hugo <victorneivadef8@gmail.com>
2023-09-04 19:59:36 -0300

    Classe Cliente implementada

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,25 @@
 package Exemplo;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
+	public static void main(String[] args) throws IOException {
+		Socket cliente = new Socket("127.0.0.1", 12345);
+		System.out.println("Cliente se conectou ao servidor!");
+		
+		Scanner teclado = new Scanner(System.in);
+		PrintStream saida = new PrintStream(cliente.getOutputStream());
+		
+		while(teclado.hasNextLine()) {
+			saida.println(teclado.nextLine());
+		}
+		
+		saida.close();
+		teclado.close();
+		cliente.close();
+	}
 
 }
